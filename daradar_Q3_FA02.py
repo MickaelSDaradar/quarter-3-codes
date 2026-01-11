459748929246722206fd3d319c2bd7b71da5f4a7
MickaelSDaradar <mickaelfermil.daradar@smc.pshs.edu.ph>
2026-01-11 04:23:34 -0800

    Create daradar_Q3_FA02.py
    top row: average daytime temperature from this Sunday to next Saturday
    bottom row: average nighttime temperature
    
    info based on my weather app's forecast feature

---
+++ daradar_Q3_FA02.py
@@ -0,0 +1,8 @@
+# lemme install my minecraft xray hacks rq
+import numpy as np
+
+# ok now lets find diamonds
+print("\n")
+arr=np.array([[31,30,31,32,31,32,33], [24,24,25,24,25,25,25]])
+print(arr)
+print(type(arr))
