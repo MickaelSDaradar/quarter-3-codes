01f196087ff7e011e35b8af38128b07710af7e26
MickaelSDaradar <mickaelfermil.daradar@smc.pshs.edu.ph>
2026-01-12 03:41:13 -0800

    Create daradar_q3_fa3.py
    1. Using an array allows you to organize data more efficiently and it's cleaner. It also allows more visible data. If you combine this with the NumPy package it'll get more organized.
    
    2. For me, I don't think there was anything difficult. Simply put, the way I found the average of the temperature for the next 7 days was by using Microsoft Excel. The hardest part may be finding the correct data to use, so I used my weather app.

---
+++ daradar_q3_fa3.py
@@ -0,0 +1,11 @@
+# how it feels like when you allow cheats in minecraft
+import numpy as np
+print("\n")
+# now back to the video
+print("\n")
+arr=np.array([[31,30,31,32,31,32,33,31], [24,24,25,24,25,25,25,25]])
+print(arr)
+print(type(arr))
+
+print("Average temperature during the day: 31")
+print("Average temperature during the night: 25")
