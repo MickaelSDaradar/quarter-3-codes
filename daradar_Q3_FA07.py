d4e7c0f39b2d3389948115ca31890f4c36a8c7a7
MickaelSDaradar <mickaelfermil.daradar@smc.pshs.edu.ph>
2026-01-27 03:33:23 -0800

    Create daradar_Q3_FA07.py

---
+++ daradar_Q3_FA07.py
@@ -0,0 +1,11 @@
+arr=[["Me: ",4500, 5200, 4800, 5000, 5300],
+     ["Lia: ",4000, 4100, 3900, 4200, 4600],
+     ["Jake: ",6000, 5800, 5900, 6100, 6200]]
+
+print(arr)
+print("Highest total: ",arr[2][5]+arr[2][4]+arr[2][3]+arr[2][2]+arr[2][1])
+print("Lowest total:",arr[1][1]+arr[1][2]+arr[1][3]+arr[1][4]+arr[1][5])
+
+print("My average number of steps: ", 4960)
+print("Lia's average number of steps: ", 4160)
+print("Jake's average number of steps: ", 6000)
