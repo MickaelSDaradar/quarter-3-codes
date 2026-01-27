aa302ecdc7162888670840dc031e440b5238ff44
MickaelSDaradar <mickaelfermil.daradar@smc.pshs.edu.ph>
2026-01-27 03:18:07 -0800

    Create daradarmickael_CS2_ActSet1.py
    hi

---
+++ daradarmickael_CS2_ActSet1.py
@@ -0,0 +1,7 @@
+thisdict = {
+}
+thisdict ["name"] = str(input("Name of student? "))
+thisdict ["age"] = str(input("How old are they? "))
+thisdict ["subject"] = str(input("What's their favourite subject? "))
+
+print(thisdict)
